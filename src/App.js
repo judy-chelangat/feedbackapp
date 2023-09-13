4c920b941a6636faa17e8a9df0cfca5b0d054555
judy-chelangat <judysigilai75@gmail.com>
2023-09-13 16:34:37 +0300

    added a delete feedback function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Header from "./components/Header"
-import FeedbackItem from "./components/FeedbackItem"
+import FeedbackList from "./components/FeedbackList"
 import { useState } from "react"
 import FeedbackData from "./data/FeedbackData"
 
 function App(){
     const [feedback,setFeedback]=useState(FeedbackData)
+
+    const deleteFeedback =(id)=>{
+        setFeedback(feedback.filter((item) => item.id!=id))
+    }
      return(
         <>
         <Header/>
         <div className="container">
-         <FeedbackItem/>
+         <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
         </div>
         </>
     )
