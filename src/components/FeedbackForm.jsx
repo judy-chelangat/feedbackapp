ae4b8df067997fd9173b8dca7749c47ab66efe3d
judy-chelangat <judysigilai75@gmail.com>
2023-09-13 18:45:06 +0300

    initial commit

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Card from './shared/Card'
+import { useState } from 'react'
+
+function FeedbackForm() {
+    const [text,setText]=useState('')
+
+    const handleTextChange =(e)=>{
+     setText(e.target.value)
+    }
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        <div className="input-group">
+            <input onChange={handleTextChange} type="text" placeholder='write a review' value={text}/>
+            <button type="submit">Send</button>
+        </div>
+      </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
