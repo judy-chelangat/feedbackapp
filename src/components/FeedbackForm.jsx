e313a16fe8c85b69c2a813b0342e2d83079e8e3a
judy-chelangat <judysigilai75@gmail.com>
2023-09-13 19:35:53 +0300

    added an if statement to handle the button and message being displayed

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import Card from './shared/Card'
 import { useState } from 'react'
+import Button from './shared/Button'
 
 function FeedbackForm() {
     const [text,setText]=useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
+  
 
     const handleTextChange =(e)=>{
+        if (text === ''){
+           setBtnDisabled (true)
+           setMessage(null)
+        }
+        else if(text != '' && text.trim().length<=10){
+            setBtnDisabled(true)
+            setMessage('text must be at least 10 characters')
+        }
+        else{
+            setMessage(null)
+            setBtnDisabled(false)
+        }
      setText(e.target.value)
     }
   return (
@@ -14,8 +30,9 @@ function FeedbackForm() {
         <h2>How would you rate your service with us?</h2>
         <div className="input-group">
             <input onChange={handleTextChange} type="text" placeholder='write a review' value={text}/>
-            <button type="submit">Send</button>
+            <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
