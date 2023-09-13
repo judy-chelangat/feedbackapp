6c900db466f656af98370d70ffcd6f499f75142d
judy-chelangat <judysigilai75@gmail.com>
2023-09-13 14:10:38 +0300

    imported feedback data and usestate hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Header from "./components/Header"
 import FeedbackItem from "./components/FeedbackItem"
+import { useState } from "react"
+import FeedbackData from "./data/FeedbackData"
 
 function App(){
+    const [feedback,setFeedback]=useState(FeedbackData)
      return(
         <>
         <Header/>
