0aeb8ba3b12895a95b4790993d98f731ade0d741
judy-chelangat <judysigilai75@gmail.com>
2024-01-21 18:43:24 +0300

    added a rating select component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import Card from './shared/Card'
+import Card from './shared/Card'//in order to wrap the form in a card
 import { useState } from 'react'
-import Button from './shared/Button'
+import Button from './shared/Button'//custom button for all components
+import RatingSelect from './RatingSelect'
 
 function FeedbackForm() {
     const [text,setText]=useState('')
-    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [rating,setRating]=useState(10) 
+    const [btnDisabled, setBtnDisabled] = useState(true) //button is disabled by default unless you type 10 characters
     const [message, setMessage] = useState('')
   
-
+  //to get whatever is being typed in
     const handleTextChange =(e)=>{
         if (text === ''){
            setBtnDisabled (true)
@@ -28,6 +30,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect/>
         <div className="input-group">
             <input onChange={handleTextChange} type="text" placeholder='write a review' value={text}/>
             <Button type="submit" isDisabled={btnDisabled}>Send</Button>
