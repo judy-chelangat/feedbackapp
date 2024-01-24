2c82b3759108f707487c0ab1b4da33fad47ae6bf
judy-chelangat <judysigilai75@gmail.com>
2024-01-24 16:41:40 +0300

    added an about page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div className='about'>
+      <h1>About this project</h1>
+      <p>React app to leave feedback for a product or service</p>
+      <p>Version:1.0.0</p>
+    </div>
+  )
+}
+
+export default AboutPage
